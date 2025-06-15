2d375480f9aadeb15ea0f9ba27842dee6f8c24cf
Diego201101 <colmop1548@gmail.com>
2025-06-15 18:42:15 -0500

    Cancion.hpp

--- Cancion.hpp
+++ Cancion.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef CANCION_HPP
 #define CANCION_HPP
 
@@ -10,14 +11,22 @@ private:
     string titulo;
     string artista;
     float duracion;
+    string genero;
 
 public:
-    Cancion(string t, string a, float d) : titulo(t), artista(a), duracion(d) {}
+    Cancion(string t, string a, float d, string g = "")
+        : titulo(t), artista(a), duracion(d), genero(g) {
+    }
 
     string getTitulo() { return titulo; }
+    string getArtista() { return artista; }
+    float getDuracion() { return duracion; }
+    string getGenero() { return genero; }
 
     void mostrar() {
-        cout << titulo << " - " << artista << " (" << duracion << " min)" << endl;
+        cout << titulo << " - " << artista << " (" << duracion << " min)";
+        if (!genero.empty()) cout << " [" << genero << "]";
+        cout << endl;
     }
 };
 
